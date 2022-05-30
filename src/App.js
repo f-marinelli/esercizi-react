e740b101d5ad028655597bc8dbd70b1700dbca75
f-marinelli <fabri.marinelli@gmail.com>
2022-05-30 09:55:10 +0200

    useEffect-03

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { useState } from 'react';
-import Counter from './Counter';
+// import { useState } from 'react';
+// import Counter from './Counter';
+import GithubUser from './GithubUser';
 
 function App() {
-  const [mount, setMount] = useState(true);
+  // const [mount, setMount] = useState(true);
 
-  const onCounterChange = (counter) => {
-    console.log(`Counter: ${counter}`);
-  };
+  // const onCounterChange = (counter) => {
+  //   console.log(`Counter: ${counter}`);
+  // };
 
-  const handleMount = () => {
-    setMount((mount) => !mount);
-  };
+  // const handleMount = () => {
+  //   setMount((mount) => !mount);
+  // };
 
   return (
     <>
-      <button onClick={handleMount}>Mount Counter</button>
-      {mount && <Counter onCounterChange={onCounterChange} />}
+      {/* <button onClick={handleMount}>Mount Counter</button>
+      {mount && <Counter onCounterChange={onCounterChange} />} */}
+      <GithubUser username="f-marinelli" />
     </>
   );
 }
