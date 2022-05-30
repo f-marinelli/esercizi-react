eea8007086654011b90b6d988ada16176da63429
f-marinelli <fabri.marinelli@gmail.com>
2022-05-30 10:44:39 +0200

    Custom Hooks - 02

---
+++ src/useForm.js
@@ -0,0 +1,20 @@
+export const useForm = () => {
+  const [form, setForm] = {
+    username: '',
+    password: '',
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+  return {
+    form,
+    onHandleInputChange: handleInputChange,
+  };
+};
