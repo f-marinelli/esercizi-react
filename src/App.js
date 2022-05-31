14b332632b40a28b2b1d75ca5004673f205f6ba0
f-marinelli <fabri.marinelli@gmail.com>
2022-05-31 09:38:52 +0200

    Router-04

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Welcome from './Welcome';
 import Counter from './Counter';
 import ShowGithubUser from './ShowGithubUser';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Welcome name="Fabrizio" />} />
-      <Route path="/counter" element={<Counter />} />
-      <Route path="/users/:username" element={<ShowGithubUser />} />
-    </Routes>
+    <>
+      <ul>
+        <li>
+          <Link to="/">Homepage</Link>
+        </li>
+        <li>
+          <Link to="/counter">Counter</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Welcome name="Fabrizio" />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/users/:username" element={<ShowGithubUser />} />
+      </Routes>
+    </>
   );
 }
 
