4fa6b01378c2a30da7265ba99c43f635039c3db7
f-marinelli <fabri.marinelli@gmail.com>
2022-05-31 09:24:30 +0200

    Router-02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Welcome from './Welcome';
+import Counter from './Counter';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Welcome name="Fabrizio" />} />
+      <Route path="/counter" element={<Counter />} />
     </Routes>
   );
 }
